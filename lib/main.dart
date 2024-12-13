7d10e96c2b0fcb4b8f62d23228269f0ddd0e5cbd
AselFernandez <aselfernandez@gmail.com>
2024-12-13 18:15:08 +0100

    simple ternary to make a login check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_app/screens/home_screen.dart';
+import 'package:qr_app/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // Get from storage
+  final bool _isLoggedIn = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,59 +19,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
-		iconTheme: const IconThemeData(
-			color: Colors.white,
-		),
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-/*
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        iconTheme: const IconThemeData(
+          color: Colors.white,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: _isLoggedIn ? const HomeScreen() : const LoginScreen(),
     );
   }
-} */
+}
